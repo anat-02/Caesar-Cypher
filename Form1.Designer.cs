2a64810f0e1ac4135ffb4066cffa0c07e6044c61
Anat <anat.hewa@gmail.com>
2020-10-06 16:49:51 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace ISSProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Encrypt = new System.Windows.Forms.Button();
+            this.tb_PlainText = new System.Windows.Forms.TextBox();
+            this.btn_Decrypt = new System.Windows.Forms.Button();
+            this.btn_BruteForce = new System.Windows.Forms.Button();
+            this.tb_CipherText = new System.Windows.Forms.TextBox();
+            this.tb_Key = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_Time = new System.Windows.Forms.TextBox();
+            this.rtb_LFA = new System.Windows.Forms.RichTextBox();
+            this.btn_LFA = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_LFAAmountSel = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Encrypt
+            // 
+            this.btn_Encrypt.Location = new System.Drawing.Point(297, 42);
+            this.btn_Encrypt.Name = "btn_Encrypt";
+            this.btn_Encrypt.Size = new System.Drawing.Size(75, 23);
+            this.btn_Encrypt.TabIndex = 1;
+            this.btn_Encrypt.Text = "Encrypt";
+            this.btn_Encrypt.UseVisualStyleBackColor = true;
+            this.btn_Encrypt.Click += new System.EventHandler(this.btn_Encrypt_Click);
+            // 
+            // tb_PlainText
+            // 
+            this.tb_PlainText.Location = new System.Drawing.Point(107, 49);
+            this.tb_PlainText.Name = "tb_PlainText";
+            this.tb_PlainText.Size = new System.Drawing.Size(165, 20);
+            this.tb_PlainText.TabIndex = 2;
+            // 
+            // btn_Decrypt
+            // 
+            this.btn_Decrypt.Location = new System.Drawing.Point(297, 124);
+            this.btn_Decrypt.Name = "btn_Decrypt";
+            this.btn_Decrypt.Size = new System.Drawing.Size(75, 23);
+            this.btn_Decrypt.TabIndex = 3;
+            this.btn_Decrypt.Text = "Decrypt";
+            this.btn_Decrypt.UseVisualStyleBackColor = true;
+            this.btn_Decrypt.Click += new System.EventHandler(this.btn_Decrypt_Click);
+            // 
+            // btn_BruteForce
+            // 
+            this.btn_BruteForce.Location = new System.Drawing.Point(297, 157);
+            this.btn_BruteForce.Name = "btn_BruteForce";
+            this.btn_BruteForce.Size = new System.Drawing.Size(75, 23);
+            this.btn_BruteForce.TabIndex = 5;
+            this.btn_BruteForce.Text = "Brute Force";
+            this.btn_BruteForce.UseVisualStyleBackColor = true;
+            this.btn_BruteForce.Click += new System.EventHandler(this.btn_BruteForce_Click);
+            // 
+            // tb_CipherText
+            // 
+            this.tb_CipherText.Location = new System.Drawing.Point(107, 126);
+            this.tb_CipherText.Name = "tb_CipherText";
+            this.tb_CipherText.Size = new System.Drawing.Size(165, 20);
+            this.tb_CipherText.TabIndex = 6;
+            // 
+            // tb_Key
+            // 
+            this.tb_Key.Location = new System.Drawing.Point(107, 86);
+            this.tb_Key.Name = "tb_Key";
+            this.tb_Key.Size = new System.Drawing.Size(100, 20);
+            this.tb_Key.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Plain Text:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Key:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Cipher Text:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Time Taken:";
+            // 
+            // tb_Time
+            // 
+            this.tb_Time.Location = new System.Drawing.Point(107, 159);
+            this.tb_Time.Name = "tb_Time";
+            this.tb_Time.Size = new System.Drawing.Size(100, 20);
+            this.tb_Time.TabIndex = 12;
+            // 
+            // rtb_LFA
+            // 
+            this.rtb_LFA.Location = new System.Drawing.Point(494, 28);
+            this.rtb_LFA.Name = "rtb_LFA";
+            this.rtb_LFA.Size = new System.Drawing.Size(665, 250);
+            this.rtb_LFA.TabIndex = 13;
+            this.rtb_LFA.Text = "";
+            // 
+            // btn_LFA
+            // 
+            this.btn_LFA.Location = new System.Drawing.Point(522, 294);
+            this.btn_LFA.Name = "btn_LFA";
+            this.btn_LFA.Size = new System.Drawing.Size(143, 27);
+            this.btn_LFA.TabIndex = 14;
+            this.btn_LFA.Text = "Letter Frequency Attack";
+            this.btn_LFA.UseVisualStyleBackColor = true;
+            this.btn_LFA.Click += new System.EventHandler(this.btn_LFA_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(690, 301);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Show the most likely";
+            // 
+            // tb_LFAAmountSel
+            // 
+            this.tb_LFAAmountSel.Location = new System.Drawing.Point(799, 298);
+            this.tb_LFAAmountSel.Name = "tb_LFAAmountSel";
+            this.tb_LFAAmountSel.Size = new System.Drawing.Size(20, 20);
+            this.tb_LFAAmountSel.TabIndex = 16;
+            this.tb_LFAAmountSel.Text = "25";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(825, 301);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "keys";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1187, 333);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tb_LFAAmountSel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_LFA);
+            this.Controls.Add(this.rtb_LFA);
+            this.Controls.Add(this.tb_Time);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_Key);
+            this.Controls.Add(this.tb_CipherText);
+            this.Controls.Add(this.btn_BruteForce);
+            this.Controls.Add(this.btn_Decrypt);
+            this.Controls.Add(this.tb_PlainText);
+            this.Controls.Add(this.btn_Encrypt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Encrypt;
+        private System.Windows.Forms.TextBox tb_PlainText;
+        private System.Windows.Forms.Button btn_Decrypt;
+        private System.Windows.Forms.Button btn_BruteForce;
+        private System.Windows.Forms.TextBox tb_CipherText;
+        private System.Windows.Forms.TextBox tb_Key;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_Time;
+        private System.Windows.Forms.RichTextBox rtb_LFA;
+        private System.Windows.Forms.Button btn_LFA;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_LFAAmountSel;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISSProject
+{
+    public partial class Form1 : Form
+    {
+
+        Dictionary<char, double> freq = new Dictionary<char, double>();
+        Dictionary<char, double>[] possible_freq = new  Dictionary<char, double>[25];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DictionaryInit();
+        }
+
+        private void btn_Encrypt_Click(object sender, EventArgs e)
+        {
+            string plaintext = tb_PlainText.Text;
+            plaintext = plaintext.Replace(" ", string.Empty); //remove spaces
+            int key = int.Parse(tb_Key.Text) % 26;
+            
+            string output = "";
+            int decOutput = 0;
+            for(int i=0; i<plaintext.Length; i++)
+            {
+                if(char.IsUpper(plaintext[i]))
+                {
+                    //convert the character to it's decimal equivalent
+                    int letter = plaintext[i];
+                    decOutput = ((letter - 65) + key) % 26;
+                    char ch = (char)(decOutput + 65);
+                    output += ch;
+                }
+                else
+                {
+                    int letter = plaintext[i];
+                    decOutput = ((letter - 97) + key) % 26;
+                    char ch = (char)(decOutput + 97);
+                    output += ch;
+                }
+            }
+            tb_CipherText.Text = output;
+        }
+
+        private void btn_Decrypt_Click(object sender, EventArgs e)
+        {
+            string ciphertext = tb_CipherText.Text;
+            ciphertext = ciphertext.Replace(" ", string.Empty); //remove spaces
+            int key = int.Parse(tb_Key.Text) % 26; //to prevent negative postions
+            key = 26 - key;
+
+            string output = "";
+            int decOutput = 0;
+            for (int i = 0; i < ciphertext.Length; i++)
+            {
+                if (char.IsUpper(ciphertext[i]))
+                {
+                    //convert the character to it's decimal equivalent
+                    int letter = ciphertext[i];
+                    decOutput = ((letter - 65) + key) % 26;
+                    char ch = (char)(decOutput + 65);
+                    output += ch;
+                }
+                else
+                {
+                    int letter = ciphertext[i];
+                    decOutput = ((letter - 97) + key) % 26;
+                    char ch = (char)(decOutput + 97);
+                    output += ch;
+                }
+            }
+            tb_PlainText.Text = output;
+        }
+
+        private void btn_BruteForce_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            string ciphertext = tb_CipherText.Text;
+            ciphertext = ciphertext.Replace(" ", string.Empty); //remove spaces
+            string[] possibles = new string[25];
+            string output = "";
+            int decOutput = 0;
+            for (int i = 1; i < 26; i++)
+            {
+                output = "";
+                for (int j = 0; j < ciphertext.Length; j++)
+                {
+                    if (char.IsUpper(ciphertext[j]))
+                    {                   
+                        int letter = ciphertext[j];
+                        decOutput = ((letter - 65) + i) % 26;
+                        char ch = (char)(decOutput + 65);
+                        output += ch;
+                        possibles[i - 1] = output;
+                    }
+                    else
+                    {
+                        int letter = ciphertext[j];
+                        decOutput = ((letter - 97) + i) % 26;
+                        char ch = (char)(decOutput + 97);
+                        output += ch;
+                        possibles[i - 1] = output;
+                    }
+                }
+            }
+
+            stopwatch.Stop();
+            tb_Time.Text = (stopwatch.Elapsed.TotalMilliseconds).ToString();
+        }
+
+        private void btn_LFA_Click(object sender, EventArgs e)
+        {
+            ResetFreq();
+            rtb_LFA.Text = "";
+            string ciphertext = tb_CipherText.Text.ToLower();
+            ciphertext = ciphertext.Replace(" ", string.Empty); //remove spaces
+            string[] possibles = new string[25];
+            double[] possibles_dist = new double[25];
+            double current_val = 0;
+            string output = "";
+            int decOutput = 0;
+            for (int i = 1; i < 26; i++)
+            {
+                output = "";
+                for (int j = 0; j < ciphertext.Length; j++)
+                {
+
+                    int letter = ciphertext[j];
+                    decOutput = ((letter - 97) + i) % 26;
+                    char ch = (char)(decOutput + 97);
+                    output += ch;
+                    possible_freq[i - 1].TryGetValue(ch, out current_val);
+                    possible_freq[i - 1][ch] = current_val + 1;
+                    possibles[i - 1] = output;
+
+                }
+            }
+            Calc_dist(possibles_dist);
+            int[] orgPos = new int[25];
+            for (int i = 0; i < 25; i++)
+            {
+                orgPos[i] = i;
+            }
+            OrderDist(possibles_dist, orgPos);
+            int show_amount = 0;
+            if (int.TryParse(tb_LFAAmountSel.Text, out show_amount)) { }
+            else show_amount = 25;
+            if (show_amount < 0)
+            {
+                show_amount *= -1;
+            }
+            else if (show_amount > 26)
+            {
+                show_amount = show_amount % 26;
+            }
+                
+            for (int i = 0; i < show_amount; i++)
+            {
+                rtb_LFA.Text += possibles[orgPos[i]] + "  Key: " + (26 - orgPos[i] - 1) + "  Distance: " + possibles_dist[i] + "\n\n";
+            }
+
+        }
+
+        private void OrderDist(double[] possibles_dist, int[] pos)
+        {
+            for(int i = 0; i <25; i++)
+            {
+                for(int j = 0; j < 25 - i - 1; j++)
+                {
+                    if(possibles_dist[j] > possibles_dist[j + 1])
+                    {
+                        double temp = possibles_dist[j];
+                        possibles_dist[j] = possibles_dist[j + 1];
+                        possibles_dist[j + 1] = temp;
+                        int temp1 = pos[j];
+                        pos[j] = pos[j + 1];
+                        pos[j + 1] = temp1;
+                    }
+                }
+            }
+            
+        }
+
+        private void Calc_dist(double[] possibles_dist)
+        {
+            for(int i = 0; i< 25; i++)
+            {
+                possibles_dist[i] =Math.Sqrt( 
+                                    Math.Pow((possible_freq[i]['e'] - freq['e']), 2)
+                                  + Math.Pow((possible_freq[i]['t'] - freq['t']), 2)
+                                  + Math.Pow((possible_freq[i]['a'] - freq['a']), 2)
+                                  + Math.Pow((possible_freq[i]['o'] - freq['o']), 2)
+                                  + Math.Pow((possible_freq[i]['i'] - freq['i']), 2)
+                                  + Math.Pow((possible_freq[i]['n'] - freq['n']), 2)
+                                  + Math.Pow((possible_freq[i]['s'] - freq['s']), 2)
+                                  + Math.Pow((possible_freq[i]['h'] - freq['h']), 2)
+                                  + Math.Pow((possible_freq[i]['r'] - freq['r']), 2)
+                                  + Math.Pow((possible_freq[i]['d'] - freq['d']), 2)
+                                  + Math.Pow((possible_freq[i]['l'] - freq['l']), 2)
+                                  + Math.Pow((possible_freq[i]['c'] - freq['c']), 2)
+                                  + Math.Pow((possible_freq[i]['u'] - freq['u']), 2)
+                                  + Math.Pow((possible_freq[i]['m'] - freq['m']), 2)
+                                  + Math.Pow((possible_freq[i]['w'] - freq['w']), 2)
+                                  + Math.Pow((possible_freq[i]['f'] - freq['f']), 2)
+                                  + Math.Pow((possible_freq[i]['g'] - freq['g']), 2)
+                                  + Math.Pow((possible_freq[i]['y'] - freq['y']), 2)
+                                  + Math.Pow((possible_freq[i]['p'] - freq['p']), 2)
+                                  + Math.Pow((possible_freq[i]['b'] - freq['b']), 2)
+                                  + Math.Pow((possible_freq[i]['v'] - freq['v']), 2)
+                                  + Math.Pow((possible_freq[i]['k'] - freq['k']), 2)
+                                  + Math.Pow((possible_freq[i]['j'] - freq['j']), 2)
+                                  + Math.Pow((possible_freq[i]['x'] - freq['x']), 2)
+                                  + Math.Pow((possible_freq[i]['q'] - freq['q']), 2)
+                                  + Math.Pow((possible_freq[i]['z'] - freq['z']), 2)
+                                  );
+                possibles_dist[i] = Math.Round(possibles_dist[i], 2);
+            }
+
+
+        }
+
+        private void DictionaryInit()
+        { 
+            freq.Add('e', 12.7020);
+            freq.Add('t', 9.0560);
+            freq.Add('a', 8.167);
+            freq.Add('o', 7.507);
+            freq.Add('i', 6.966);
+            freq.Add('n', 6.749);
+            freq.Add('s', 6.3270);
+            freq.Add('h', 6.094);
+            freq.Add('r', 5.987);
+            freq.Add('d', 4.253);
+            freq.Add('l', 4.025);
+            freq.Add('c', 2.782);
+            freq.Add('u', 2.758);
+            freq.Add('m', 2.406);
+            freq.Add('w', 2.36);
+            freq.Add('f', 2.228);
+            freq.Add('g', 2.015);
+            freq.Add('y', 1.974);
+            freq.Add('p', 1.929);
+            freq.Add('b', 1.492);
+            freq.Add('v', 0.978);
+            freq.Add('k', 0.772);
+            freq.Add('j', 0.153);
+            freq.Add('x', 0.15);
+            freq.Add('q', 0.095);
+            freq.Add('z', 0.074);
+            for(int i = 0; i < 25; i++)
+            {
+                possible_freq[i] = new Dictionary<char, double>();
+                possible_freq[i].Add('e', 0);
+                possible_freq[i].Add('t', 0);
+                possible_freq[i].Add('a', 0);
+                possible_freq[i].Add('o', 0);
+                possible_freq[i].Add('i', 0);
+                possible_freq[i].Add('n', 0);
+                possible_freq[i].Add('s', 0);
+                possible_freq[i].Add('h', 0);
+                possible_freq[i].Add('r', 0);
+                possible_freq[i].Add('d', 0);
+                possible_freq[i].Add('l', 0);
+                possible_freq[i].Add('c', 0);
+                possible_freq[i].Add('u', 0);
+                possible_freq[i].Add('m', 0);
+                possible_freq[i].Add('w', 0);
+                possible_freq[i].Add('f', 0);
+                possible_freq[i].Add('g', 0);
+                possible_freq[i].Add('y', 0);
+                possible_freq[i].Add('p', 0);
+                possible_freq[i].Add('b', 0);
+                possible_freq[i].Add('v', 0);
+                possible_freq[i].Add('k', 0);
+                possible_freq[i].Add('j', 0);
+                possible_freq[i].Add('x', 0);
+                possible_freq[i].Add('q', 0);
+                possible_freq[i].Add('z', 0);
+
+            }
+        }
+
+        private void ResetFreq()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                possible_freq[i]['e'] = 0;
+                possible_freq[i]['t'] = 0;
+                possible_freq[i]['a'] = 0;
+                possible_freq[i]['o'] = 0;
+                possible_freq[i]['i'] = 0;
+                possible_freq[i]['n'] = 0;
+                possible_freq[i]['s'] = 0;
+                possible_freq[i]['h'] = 0;
+                possible_freq[i]['r'] = 0;
+                possible_freq[i]['d'] = 0;
+                possible_freq[i]['l'] = 0;
+                possible_freq[i]['c'] = 0;
+                possible_freq[i]['u'] = 0;
+                possible_freq[i]['m'] = 0;
+                possible_freq[i]['w'] = 0;
+                possible_freq[i]['f'] = 0;
+                possible_freq[i]['g'] = 0;
+                possible_freq[i]['y'] = 0;
+                possible_freq[i]['p'] = 0;
+                possible_freq[i]['b'] = 0;
+                possible_freq[i]['v'] = 0;
+                possible_freq[i]['k'] = 0;
+                possible_freq[i]['j'] = 0;
+                possible_freq[i]['x'] = 0;
+                possible_freq[i]['q'] = 0;
+                possible_freq[i]['z'] = 0;
+                
+            }
+
+        }
+
+        
+    }
+}
